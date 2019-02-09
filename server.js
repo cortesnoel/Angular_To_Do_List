1ebedc0c28f31823dbbc4c08a6d3359f3088107e
Noel Cortes <noelcortes472640@gmail.com>
2019-02-09 14:31:50 -0500

    fixing relative paths in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/t-o-d-o-list'));
+app.use(express.static(__dirname + '/dist/todo'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/todo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
